d2d1f6d77abaa2aa8acb1ee8e5e727d9b6c3cef1
Gerald McGarvin <gerald.mcgarvin@hotmail.com>
2017-04-03 14:10:21 -0500

    Assignment 2 added more comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,21 @@
 ## R Programming Assignment 2
 ## Gerald McGarvin April 3, 2017
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Call makeCacheMatrix to store a matrix and its inverse, if setInv is called.
+## makeCacheMatrix expects a square matrix as input.
+## It returns an R list object containing get and set functions provided by makeCacheMatrix.
+## setOrig() can be called to define the original matrix in makeCacheMatrix.
+## setInv() can be called to store the inverses of the original matrix in makeCacheMatrix.
+## getOrig() returns the original matrix.
+## getInv() returns the inverse of the original matrix.
+## Test usintg the following statements in RStudio:
+## > source("cachematrix.R")
+## > m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+## > my_matrix <- makeCacheMatrix(m1)  ## creates a makeCacheMatrix object containing m1 matrix.
+## > cacheSolve(my_matrix)
+## The first time the last statement is executed the inverse matrix is displayed.
+## If repeated for the same input matrix, the inverse matrix is displayed along with the following message:
+## "Retrieved inverse of matrix from cache"
 
 makeCacheMatrix <- function(origX = matrix()) {
 	invX <- NULL					## clear variable when cache is instantiated.
@@ -20,10 +32,9 @@ makeCacheMatrix <- function(origX = matrix()) {
 	list( setOrig = setOrig, getOrig = getOrig, setInv = setInv, getInv = getInv)
 }
 
-
 ## Call cacheSolve passing in a makeCacheMatrix object.
 ## Return the inverse of the matrix, origX already in the makeCacheMatrix object.
-## Call getters and setters in makeCacheMatrix as needed.
+## Call get and set functions in makeCacheMatrix as needed.
 
 cacheSolve <- function(x, ...) {	## pass a makeCacheMatrix object.
 		invX <- x$getInv()  	## attempt to get cached inverse of x, the original matrix.
